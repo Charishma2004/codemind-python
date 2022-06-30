b96d15e610833424197ca556d6f3cad3238b0187
Charishma2004 <98752245+Charishma2004@users.noreply.github.com>
2022-06-30 23:33:32 +0530

    s1=input() s2=input() n=s1.lower() m=s2.lower() s3=n.split() s4=m.split() for i in s4:     if i in s3:         print(i,end=' ')

--- common_words.py
+++ common_words.py
@@ -1,26 +1,11 @@
-a=input()
-b=input()
-a1=""
-b1=""
-for i in a:
-    if i.isupper():
-        a1+=i.lower()
-    else:
-        a1+=i
-for i in b:
-    if i.isupper():
-        b1+=i.lower()
-    else:
-        b1+=i
-arr=list(a1.split())
-brr=list(b1.split())
-res=[]
-for i in brr:
-    if i in arr:
-        if i not in res:
-            res.append(i)
-if len(res)==0:
-    print(0)
-else:
-    for i in res:
-        print(i,end=" ")
+s1=input()
+s2=input()
+n=s1.lower()
+m=s2.lower()
+s3=n.split()
+s4=m.split()
+for i in s4:
+    if i in s3:
+        print(i,end=' ')
+    
+    
